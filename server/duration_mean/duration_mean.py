e94a43c5e582214a2089bdcec0e4c7f9e083298f
MauRodriguez <mrodriguezb@fi.uba.ar>
2023-05-12 18:08:59 -0300

    duration_mean_result_joiner

--- server/duration_mean/duration_mean.py
+++ server/duration_mean/duration_mean.py
@@ -3,17 +3,33 @@ import sys
 sys.path.append("..")
 from rabbitmq.rabbit import Rabbitmq
 END = "E"
+MAX_MSG_LENGHT = 8192
 
 class DurationMean:
     def __init__(self):
         self.rabbit = Rabbitmq()
-        self.rainy_trips = {}    
+        self.trips_duration = {} 
+
+    def send_partial_duration(self):
+        response = ""
+        
+        for day in self.trips_duration:  
+            duration = str(self.trips_duration[day][0])
+            trips = str(self.trips_duration[day][1])
+            new_data = day + "," + duration + "," + trips + ";"
+            if len(response + new_data) > MAX_MSG_LENGHT:
+                self.rabbit.publish("","duration_mean", response)
+                response = ""
+            response += new_data
+                
+        self.rabbit.publish("", "duration_mean", END)   
 
     def callback(self, ch, method, properties, body):
         body = body.decode('utf-8')
         
         if body == END:
             logging.info("End of rainy trips received")
+            self.send_partial_duration()
             ch.close()
             return
     
@@ -22,11 +38,11 @@ class DurationMean:
         for row in rows:
             cols = row.split(',')
             if len(cols) < 2: continue
-            if cols[0] not in self.rainy_trips:
-                self.rainy_trips[cols[0]] = (float(cols[1]), 1)
+            if cols[0] not in self.trips_duration:
+                self.trips_duration[cols[0]] = (float(cols[1]), 1)
 
-            current = self.rainy_trips[cols[0]]
-            self.rainy_trips[cols[0]] = (current[0] + float(cols[1]), current[1] + 1)
+            current = self.trips_duration[cols[0]]
+            self.trips_duration[cols[0]] = (current[0] + float(cols[1]), current[1] + 1)
 
     def run(self):
         self.rabbit.consume("rainy_trips", self.callback)
--- server/duration_mean_joiner/duration_mean_joiner.py
+++ server/duration_mean_joiner/duration_mean_joiner.py
@@ -4,35 +4,34 @@ sys.path.append("..")
 from rabbitmq.rabbit import Rabbitmq
 END = "E"
 MAX_MSG_LENGHT = 8192
-QUERY = "Q3"
 
-class DistanceMeanJoiner:
+class DurationMeanJoiner:
     def __init__(self):
         self.rabbit = Rabbitmq()
-        self.distances = {}    
+        self.duration = {}    
 
-    def calculate_mean_distance(self):
-        response = f"{QUERY};"
+    def calculate_mean_duration(self):
+        response = ""
         
-        for city in self.distances:  
-            distance = self.distances[city][0]
-            trips = self.distances[city][1]
+        for city in self.duration:  
+            distance = self.duration[city][0]
+            trips = self.duration[city][1]
             mean = distance / trips
             if mean < 6: continue
             new_data = city + ";"
             if len(response + new_data) > MAX_MSG_LENGHT:
                 self.rabbit.publish("","distance_join", response)
-                response = f"{QUERY};"
+                response = ""
             response += new_data
         
-        self.rabbit.publish("", "result", response)                
-        self.rabbit.publish("", "result", QUERY + "," + END + ";" )
+        self.rabbit.publish("", "distance_result", response)                
+        self.rabbit.publish("", "distance_result", END)
 
     def callback(self, ch, method, properties, body):
         body = body.decode('utf-8')
         if body == END:
             logging.info("End of distance mean received")
-            self.calculate_mean_distance()
+            self.calculate_mean_duration()
             ch.close()
             return
     
@@ -40,12 +39,12 @@ class DistanceMeanJoiner:
         for row in rows:
             cols = row.split(',')
             if len(cols) < 3: continue
-            if cols[0] not in self.distances:
-                self.distances[cols[0]] = (float(cols[1]), int(cols[2]))
+            if cols[0] not in self.duration:
+                self.duration[cols[0]] = (float(cols[1]), int(cols[2]))
                 continue           
 
-            self.distances[cols[0]] = (self.distances[cols[0]][0] + float(cols[1]), self.distances[cols[0]][1] + int(cols[2]))
+            self.duration[cols[0]] = (self.duration[cols[0]][0] + float(cols[1]), self.duration[cols[0]][1] + cols[2])
 
     def run(self):
-        self.rabbit.consume("distance_mean", self.callback)
+        self.rabbit.consume("duration_mean", self.callback)
         self.rabbit.close()      
