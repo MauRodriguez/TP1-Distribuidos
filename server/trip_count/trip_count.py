0162a3c3284b82f6990840321439fbb5886a412e
MauRodriguez <mrodriguezb@fi.uba.ar>
2023-05-13 11:51:06 -0300

    trip counter result joiner

--- server/trip_count/trip_count.py
+++ server/trip_count/trip_count.py
@@ -14,11 +14,10 @@ class TripCount :
     def send_partial_count(self):
         response = ""
         
-        for count in self.counts:  
-            city = count[0]
-            year = count[1]
-            trips = str(self.counts[count])
-            new_data = city + "," + year + "," + trips + ";"
+        for city in self.counts:  
+            trips_16 = str(self.counts[city][0])
+            trips_17 = str(self.counts[city][1])
+            new_data = city + "," + trips_16 + "," + trips_17 + ";"
             if len(response + new_data) > MAX_MSG_LENGHT:
                 self.rabbit.publish("","trip_count", response)
                 response = ""
@@ -31,7 +30,6 @@ class TripCount :
         body = body.decode('utf-8')
         if body == END:
             logging.info("End of 16 17 trips received")
-            logging.info(self.counts)
             self.send_partial_count()
             ch.close()
             
@@ -39,10 +37,16 @@ class TripCount :
         for row in rows:
             cols = row.split(',')
             if len(cols) < 2: continue
-            if (self.stations[cols[1]],cols[0]) not in self.counts:
-                self.counts[(self.stations[cols[1]],cols[0])] = 1
-                continue
-            self.counts[(self.stations[cols[1]],cols[0])] += 1
+            if self.stations[cols[1]] not in self.counts:
+                self.counts[self.stations[cols[1]]] = [0,0]
+
+            trips_16 = self.counts[self.stations[cols[1]]][0]
+            trips_17 = self.counts[self.stations[cols[1]]][1]
+
+            if cols[0] == "2016":
+                self.counts[self.stations[cols[1]]][0] = trips_16 + 1
+            elif cols[0] == "2017":
+                self.counts[self.stations[cols[1]]][1] = trips_17 + 1
 
     def callback_stations(self, ch, method, properties, body):
         body = body.decode('utf-8')
---
+++ server/trip_count_joiner/trip_count_joiner.py
@@ -0,0 +1,51 @@
+import logging
+import sys
+sys.path.append("..")
+from rabbitmq.rabbit import Rabbitmq
+END = "E"
+MAX_MSG_LENGHT = 8192
+QUERY = "Q2"
+
+class TripCountJoiner:
+    def __init__(self):
+        self.rabbit = Rabbitmq()
+        self.count = {} 
+
+    def calculate_count_duplicate(self):
+        response = f"{QUERY};"
+
+        for city in self.count:  
+            trips_16 = self.count[city][0]
+            trips_17 = self.count[city][1]
+            if (2 * trips_16) > trips_17: continue
+            new_data = city + ";"
+            if len(response + new_data) > MAX_MSG_LENGHT:
+                self.rabbit.publish("","result", response)
+                response = f"{QUERY};"
+            response += new_data
+        
+        self.rabbit.publish("", "result", response)                
+        self.rabbit.publish("", "result", QUERY + "," + END + ";")
+
+    def callback(self, ch, method, properties, body):
+        body = body.decode('utf-8')
+        if body == END:
+            logging.info("End of trip partial count received")
+            self.calculate_count_duplicate()
+            ch.close()
+            return
+    
+        rows = body.split(';')
+        for row in rows:
+            cols = row.split(',')
+            if len(cols) < 3: continue
+            if cols[0] not in self.count:
+                self.count[cols[0]] = [0,0]                           
+            trips_16 = self.count[cols[0]][0]
+            trips_17 = self.count[cols[0]][1]
+
+            self.count[cols[0]] = [trips_16 + int(cols[1]), trips_17 + int(cols[2])]
+
+    def run(self):
+        self.rabbit.consume("trip_count", self.callback)
+        self.rabbit.close()      
